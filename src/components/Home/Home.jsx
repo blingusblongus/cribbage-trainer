41133d678fcdaeb1770627956244decafb68b0c4
Nick Allen <rutherford.t.b@gmail.com>
2021-11-17 10:41:37 -0600

    add basic buttons to homepage

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import './Home.css';
+
+function Home(props) {
+    return (
+        <>
+        <div>Home Page</div>
+        <div className="primary-btn">Cribbage Golf</div>
+        <div className="primary-btn">Profile</div>
+        <div className="primary-btn">Leaderboards</div>
+        <div className="primary-btn">Logout</div>
+        </>
+    )
+}
+
+export default Home;
