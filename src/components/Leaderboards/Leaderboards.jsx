103f97f48d3a773859618b38de5e06fbf2b9a7cc
Nick Allen <rutherford.t.b@gmail.com>
2021-11-17 17:25:26 -0600

    leaderboards appear on dom

--- src/components/Leaderboards/Leaderboards.jsx
+++ src/components/Leaderboards/Leaderboards.jsx
@@ -8,46 +8,52 @@ import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 
+import './Leaderboards.css';
+
 function Leaderboards(props) {
     const dispatch = useDispatch();
     const allScores = useSelector(store => store.allScores);
-    
+
     useEffect(() => {
-        dispatch({type: 'GET_LEADERBOARD_SCORES'})
-    },[])
+        dispatch({ type: 'GET_LEADERBOARD_SCORES' })
+    }, [])
 
-    return (<>
-        <div>Leaderboards</div>
+    return (
+        <div className="leaderboard">
+            <div>Leaderboards</div>
 
-        <TableContainer>
-            <Table aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Rank</TableCell>
-                        <TableCell align="right">Name</TableCell>
-                        <TableCell align="center">Score</TableCell>
-                        <TableCell align="right">Date</TableCell>
-                        
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {allScores.map((row, i) => (
-                        <TableRow
-                            key={row.id}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                        >
-                            <TableCell component="th" scope="row">
-                                {i + 1}
-                            </TableCell>
-                            <TableCell align="right">{row.display_name}</TableCell>
-                            <TableCell align="right">{row.golf_score}</TableCell>
-                            <TableCell align="right">{row.timestamp.split('T')[0]}</TableCell>
+            <TableContainer>
+                <Table aria-label="simple table"
+                    sx={{
+                        color: 'white',
+                      }}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Rank</TableCell>
+                            <TableCell align="right">Name</TableCell>
+                            <TableCell align="center">Score</TableCell>
+                            <TableCell align="right">Date</TableCell>
+
                         </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-    </>
+                    </TableHead>
+                    <TableBody>
+                        {allScores.map((row, i) => (
+                            <TableRow
+                                key={row.id}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell component="th" scope="row">
+                                    {i + 1}
+                                </TableCell>
+                                <TableCell align="right">{row.display_name}</TableCell>
+                                <TableCell align="right">{row.golf_score}</TableCell>
+                                <TableCell align="right">{row.timestamp.split('T')[0]}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div>
     )
 }
 
