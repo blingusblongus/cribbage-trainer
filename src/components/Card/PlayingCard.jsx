d7a1a828cb53ab00257138dc4147ae08a66812fb
Nick Allen <rutherford.t.b@gmail.com>
2021-11-18 17:18:15 -0600

    removed old logs, added box-shadow to cards

--- src/components/Card/PlayingCard.jsx
+++ src/components/Card/PlayingCard.jsx
@@ -1,14 +1,10 @@
 import './PlayingCard.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { Card } from '@mui/material';
 
 function playingCard({card, noSelect}){
     const dispatch = useDispatch();
     const hand = useSelector(store => store.hand);
     const selected = hand.filter(id => id === card.id).length > 0;
-    console.log('rerender')
-    console.log(hand);
-    console.log(selected);
 
     const addToHand = () => {
         //max 4 cards selected
