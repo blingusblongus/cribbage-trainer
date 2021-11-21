a9c20e9de0b875db5e15ec8ff7d23586c52b1b42
Nick Allen <rutherford.t.b@gmail.com>
2021-11-20 22:15:39 -0600

    LearnMode keeps track of already found scores

--- src/components/LearnMode/LearnMode.jsx
+++ src/components/LearnMode/LearnMode.jsx
@@ -14,12 +14,52 @@ function LearnMode(props) {
     const golfRounds = useSelector(store => store.global.golfRounds);
     const [displayResults, setDisplayResults] = useState(false);
     const flip = deal.filter(card => card.flip)[0] || null;
+    const singleHandCheck = useSelector(store => store.singleHandCheck);
+    const hand = useSelector(store => store.hand);
+    const [foundScores, setFoundScores] = useState([]);
+    console.log(singleHandCheck);
+    console.log(hand);
+
+    const checkSelected = () => {
+        if (hand.length === 0) return;
+
+        //check if score was already found
+        for (let score of foundScores) {
+            if (hand.length === score.length
+                && score.filter(card => hand.includes(card.id))) {
+
+                console.log('match already found');
+                return;
+            }
+        }
+
+        const scores = singleHandCheck.scores;
+
+        //iterate through all stored scoring hands
+        for (let scoreType in scores) {
+            for (let combo of scores[scoreType]) {
+                //check selection against scoring hand
+                if (hand.length === combo.length
+                    && combo.filter(card => hand.includes(card.id))) {
+
+                    console.log('match', scores[scoreType]);
+                    setFoundScores([...foundScores, combo]);
+                }
+
+            }
+        }
+    }
 
     // Deal cards on page load
     useEffect(() => {
         dispatch({ type: 'NEW_HAND' });
         dispatch({ type: 'DEAL', payload: 5 });
     }, []);
+
+    // run checkSelected when hand is updated
+    useEffect(() => {
+        checkSelected();
+    }, [hand]);
 
     // Deal a new hand
     const newHand = () => {
@@ -33,7 +73,6 @@ function LearnMode(props) {
         setShowChart(false);
         dispatch({ type: 'DEAL', payload: 6 });
     }
-
 
     // CONDITIONAL RENDERING ==========
 
