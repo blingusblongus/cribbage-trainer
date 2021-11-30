c3b042db4e6a3f73e57f2f00c1036471a82a7976
Nick Allen <rutherford.t.b@gmail.com>
2021-11-30 09:37:06 -0600

    changed login title to 'Cribbage Trainer'

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -15,16 +15,16 @@ function Nav() {
   console.log(location)
 
   const redirect = (path) => {
-    if(path === location.pathname) return;
+    if (path === location.pathname) return;
     history.push(path);
-}
+  }
 
   const parsePathname = (pathname) => {
-    if(!user.id && pathname !== '/leaderboards'){
-      return 'Log In';
-    } 
-    
-    switch(pathname){
+    if (!user.id && pathname !== '/leaderboards') {
+      return 'Cribbage Trainer';
+    }
+
+    switch (pathname) {
       case '/home': return 'Home';
       case '/golf': return 'Cribbage Golf';
       case '/leaderboards': return 'Leaderboards';
@@ -36,87 +36,89 @@ function Nav() {
 
   return (
     <>
-    {user.id === null ?
-    <div className="nav">
-      <Link to="/home">
-        <h2 className="nav-title">Cribbage Trainer</h2>
-      </Link>
-      <div>
-        {/* If no user is logged in, show these links */}
-        {user.id === null &&
-          // If there's no user, show login/registration links
-          <>
-            <Link className="navLink" to="/login">
-              Login / Register
-            </Link>
+      {user.id === null ?
+        <div className="nav">
+          <Link to="/home">
+            <h2 className="nav-title">Cribbage Trainer</h2>
+          </Link>
+          <div>
+            {/* If no user is logged in, show these links */}
+            {user.id === null &&
+              // If there's no user, show login/registration links
+              <>
+                <Link className="navLink" to="/login">
+                  Login / Register
+                </Link>
 
-            <Link className="navLink" to="/leaderboards">
-              Leaderboards
-            </Link>
-          </>
-        }
+                <Link className="navLink" to="/leaderboards">
+                  Leaderboards
+                </Link>
+              </>
+            }
 
-        {/* If a user is logged in, show these links */}
-        {user.id && (
-          <>
-            <Link className="navLink" to="/user">
-              Profile
-            </Link>
-            {/* 
+            {/* If a user is logged in, show these links */}
+            {user.id && (
+              <>
+                <Link className="navLink" to="/user">
+                  Profile
+                </Link>
+                {/* 
             <Link className="navLink" to="/info">
               Info Page
             </Link> */}
 
-            <Link className="navLink" to="/golf">
-              Cribbage Golf
-            </Link>
+                <Link className="navLink" to="/golf">
+                  Cribbage Golf
+                </Link>
 
-            <Link className="navLink" to="/leaderboards">
-              Leaderboards
-            </Link>
+                <Link className="navLink" to="/leaderboards">
+                  Leaderboards
+                </Link>
 
-            <LogOutButton className="navLink" />
-          </>
-        )}
+                <LogOutButton className="navLink" />
+              </>
+            )}
 
-        {/* <Link className="navLink" to="/about">
+            {/* <Link className="navLink" to="/about">
           About
         </Link> */}
 
-      </div>
-    </div>
+          </div>
+        </div>
 
-    :
+        :
 
-    <>
-    <nav id="top-bar" className="nav-bar">
-        <h1 className="page-title">{parsePathname(location.pathname)}</h1>
-    </nav>
+        <>
+          <nav id="top-bar" className="nav-bar">
+            <h1 className="page-title">
+              {parsePathname(location.pathname)}
+            </h1>
+          </nav>
 
-    <nav className="nav-bar bottom-bar">
-          <div 
-            className="nav-icon flex-grow text-center bar-item"
-            onClick={() => redirect('/home')}>
-            <HomeOutlinedIcon fontSize={'large'}/>
-          </div>
-          <div id="card-icon-container" 
-            className="nav-icon flex-grow text-center bar-item"
-            onClick={() => redirect('/golf')}>
-            <img id="custom-card-icon" src={CardIcon}/>
-          </div>
-          <div 
-            className="nav-icon flex-grow text-center bar-item"
-            onClick={() => redirect('/user')}>
-            <PersonOutlineOutlinedIcon fontSize={'large'}/>
-          </div>
-          <div 
-            className="nav-icon flex-grow text-center bar-item"
-            onClick={() => redirect('/leaderboards')}>
-            <LeaderboardOutlinedIcon fontSize={'large'}/>
-          </div>
-        </nav>
+          <nav className="nav-bar bottom-bar">
+            <div
+              className="nav-icon flex-grow text-center bar-item"
+              onClick={() => redirect('/home')}>
+              <HomeOutlinedIcon fontSize={'large'} />
+            </div>
+            <div id="card-icon-container"
+              className="nav-icon flex-grow text-center bar-item"
+              onClick={() => redirect('/golf')}>
+              <img id="custom-card-icon" src={CardIcon} />
+            </div>
+            <div
+              className="nav-icon flex-grow text-center bar-item"
+              onClick={() => redirect('/user')}>
+              <PersonOutlineOutlinedIcon fontSize={'large'} />
+            </div>
+            <div
+              className="nav-icon flex-grow text-center bar-item"
+              onClick={() => redirect('/leaderboards')}>
+              <LeaderboardOutlinedIcon fontSize={'large'} />
+            </div>
+          </nav>
         </>
-}
+      }
     </>
   );
 }
